0498b3ab9839effbc9c8128335c35e8923deff08
Christopher Amanor <christopher.k.amanor@gmail.com>
2023-05-30 13:04:46 +0000

    Add carousel to section

--- script.js
+++ script.js
@@ -30,3 +30,31 @@ const sectionObserver = new IntersectionObserver((entries, observer) => {
 sections.forEach((section) => {
   sectionObserver.observe(section);
 });
+
+const carousel = document.querySelector(".carousel");
+const carouselItems = document.querySelectorAll(".carousel-item");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentIndex = 0;
+
+function slideCarousel() {
+  const itemWidth = carouselItems[currentIndex].offsetWidth;
+  carousel.style.transform = `translateX(${-itemWidth * currentIndex}px)`;
+}
+
+function nextItem() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  slideCarousel();
+}
+
+function previousItem() {
+  currentIndex =
+    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  slideCarousel();
+}
+
+prevBtn.addEventListener("click", previousItem);
+nextBtn.addEventListener("click", nextItem);
+
+document.addEventListener("DOMContentLoaded", slideCarousel);
