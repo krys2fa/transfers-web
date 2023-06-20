a5df6c89d4d197c6f160bbb4d6f80f679019859b
Christopher Amanor <camanor@webfx.com>
2023-06-20 02:06:52 +0000

    Implement carousel for mobile view

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+// Carousel functionality
+const carousel = document.querySelector(".carousel");
+const carouselItems = document.querySelectorAll(".carousel-item");
+
+let currentIndex = 0;
+const itemsToShow = 1;
+let timer;
+
+function slideCarousel() {
+  if (carousel) {
+    const itemWidth = carouselItems[currentIndex].offsetWidth;
+    const transformValue = -itemWidth * currentIndex;
+    carousel.style.transform = `translateX(${transformValue}px)`;
+  }
+}
+
+const startCarouselTimer = () => {
+  timer = setInterval(() => {
+    nextItem();
+  }, 3000);
+};
+
+const stopCarouselTimer = () => {
+  clearInterval(timer);
+};
+
+if (carousel) {
+  carousel.addEventListener("mouseenter", stopCarouselTimer);
+  carousel.addEventListener("mouseleave", startCarouselTimer);
+}
