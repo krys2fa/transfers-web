5e52a5ece1f939e1246ac8827db59aab7f4dd2a5
Christopher Amanor <camanor@webfx.com>
2023-06-20 14:20:38 +0000

    Format text and maps

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
+// const hamburger = document.querySelector(".hamburger");
+// const menu = document.querySelector(".menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  menu.classList.toggle("active");
-});
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   menu.classList.toggle("active");
+// });
 
 // Carousel functionality
 const carousel = document.querySelector(".carousel");
@@ -41,3 +41,39 @@ if (carousel) {
   carousel.addEventListener("mouseenter", stopCarouselTimer);
   carousel.addEventListener("mouseleave", startCarouselTimer);
 }
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu a");
+const sections = document.querySelectorAll(".section");
+
+function toggleMenu() {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  menu.classList.remove("active");
+}
+
+function scrollToSection(e) {
+  e.preventDefault();
+  closeMenu();
+  const targetId = e.target.getAttribute("href");
+  const targetSection = document.querySelector(targetId);
+  if (targetSection) {
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+sections.forEach((section) => {
+  section.addEventListener("click", closeMenu);
+});
