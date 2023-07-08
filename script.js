189c526f2a20566c1c19b067d3d2d2e11bade025
Christopher Amanor <camanor@webfx.com>
2023-07-08 22:08:03 +0000

    Update about.html

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".menu li a");
+  const links = document.querySelectorAll(".nav-link");
 
   links.forEach(function (link) {
     if (link.href === window.location.href) {
+      console.log("🚀 ~ file: script.js:9 ~ link:", link);
+
       link.style.color = "#fff";
     }
   });
