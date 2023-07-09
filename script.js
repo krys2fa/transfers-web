023d7dabebcbeec20d3553999350a1e803681cb6
Christopher Amanor <christopher.k.amanor@gmail.com>
2023-07-09 15:36:08 +0000

    Update gallery.html

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $(".clickable-image").on("click", function () {
+    var imageSrc = $(this).data("image-src");
+    $("#imageModal").find(".maximized-image").attr("src", imageSrc);
+  });
+});
