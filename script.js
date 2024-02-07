4d41154874c1d3a094aeb6764c4d2644f91803f1
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-02-07 17:40:22 +0000

    Rearrange photos in osnabruck gallery and move captions to modal

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function () {
   $(".img").click(function () {
     $("#modal-img").attr("src", this.src);
-    $("#imageModalLabel").text("TRANSFER(S): From Osnabrück to Tamale, Public programme at Red Clay, November 28-1 December 2023, " +
-    decodeURIComponent(this.src.substring(this.src.indexOf('Photo'), this.src.indexOf("_"))));
+    $("#imageModalLabel").text(
+      "TRANSFER(S): From Osnabrück to Tamale, Public programme at Red Clay, November 28-1 December 2023, " +
+        decodeURIComponent(
+          this.src.substring(this.src.indexOf("Photo"), this.src.indexOf("_"))
+        )
+    );
+    $("#imageModalCaption").text(
+      "TRANSFER(S), public installation by Ibrahim Mahama, 2023, co-curated by Kwasi Ohene-Ayeh and Bettina Klein, commissioned by Kunsthalle Osnabrück, Photo " +
+        decodeURIComponent(
+          this.src.substring(this.src.indexOf("by"), this.src.indexOf(".jpeg"))
+        ).replace(/[0-9]/g, "")
+    );
     $("#imageModal").modal("show");
   });
 });
